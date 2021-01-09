fee5779d5767da54556f16d9e50ba11a3f844a6b
Eunovo <eunovo9@gmail.com>
2021-01-09 19:48:31 +0100

    create seller or rider after user

--- server/src/controllers/users/UserController.ts
+++ server/src/controllers/users/UserController.ts
@@ -1,4 +1,4 @@
-import { services } from "../../backend"; 
+import { services } from "../../backend";
 import { BaseController } from "../BaseController";
 
 export class UserController extends BaseController {
@@ -11,10 +11,19 @@ export class UserController extends BaseController {
     }
 
     private async create(req: any) {
-        await services.User.create(req.body);
+        const id = await services.User.create(req.body);
+        const { role } = req.body;
+        const extension = { user: id, ...req.body };
+        if (role === 'rider')
+            await services.Rider.create(extension);
+        else if (role === 'seller')
+            await services.Seller
+                .create({ ...extension, approved: false });
+
         return { message: "success" }
     }
 
+    // TODO update seller or rider too
     private async update(req: any) {
         await services.User.updateOne(req.body, req.query);
         return { message: "success" }
