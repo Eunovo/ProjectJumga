16694712982a4bb9739ccbaacd657342ccbe34b3
Eunovo <eunovo9@gmail.com>
2021-01-09 13:49:25 +0100

    Complete useLogin

--- client/src/hooks/users/login.tsx
+++ client/src/hooks/users/login.tsx
@@ -1,9 +1,18 @@
 import { useMutate } from '../../api';
+import { useCurrentUser } from '../../state/AppState';
 
 export const useLogin = () => {
+    const { setUser } = useCurrentUser();
     const { mutate, ...state } = useMutate('/auth/login', 'post');
 
-    const login = (data: any) => mutate(data);
+    const login = async (data: any) => {
+        const response = await mutate(data);
+        if (!response || !setUser)
+            throw new Error('An unexpected error occurred');
+
+        const { token, user } = response.data;
+        setUser({ ...user, token });
+    };
 
     return { login, ...state };
 }
