36bd298b0dac7aed6024d3699861c6587dadce90
Eunovo <eunovo9@gmail.com>
2021-01-09 19:13:51 +0100

    Use port defined in environment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import './config';
 import { app } from './app';
 
-app.listen(5000, () => {
-    console.log(`Listening on port 5000`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
