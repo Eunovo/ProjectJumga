a479863804cfa9381e5bd16c6604cecbce1adcfa
Eunovo <eunovo9@gmail.com>
2021-01-10 17:01:07 +0100

    Proper error handling

--- client/src/hooks/users/approve-store.tsx
+++ client/src/hooks/users/approve-store.tsx
@@ -7,15 +7,24 @@ export const useApproveStore = () => {
     const { get, ...state } = useLazyGet('/users/approve');
 
     const approveStore = async () => {
-        const response = await get();
-        const link = response?.data?.paymentLink;
-        
-        if (!link) {
-            enqueueSnackbar(
-                `Could not complete payment`, { variant: 'error' });
-        }
+        try {
+            const response = await get();
+            const link = response?.data?.paymentLink;
 
-        window.open(link, '_blank');
+            if (!link) {
+                enqueueSnackbar(
+                    `Could not complete payment: ${response?.message}`,
+                    { variant: 'error' }
+                );
+            }
+
+            window.open(link, '_blank');
+        } catch (error) {
+            enqueueSnackbar(
+                `Could not complete payment: ${error.message}`,
+                { variant: 'error' }
+            );
+        }
     }
 
     return { ...state, approveStore };
