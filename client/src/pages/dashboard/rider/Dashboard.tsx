bc2a87d081ac97f310dff5edc2fd7f37a2bfa934
Eunovo <eunovo9@gmail.com>
2021-01-13 20:33:14 +0100

    Show rider earnings

--- client/src/pages/dashboard/rider/Dashboard.tsx
+++ client/src/pages/dashboard/rider/Dashboard.tsx
@@ -1,11 +1,14 @@
 import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { useStyles } from "../styles";
 import { SetupAccountAlert } from '../SetupAcccountAlert';
 import { RiderPage } from "./RiderPage";
+import { useCurrentUser } from "../../../state/AppState";
 
 
 export const RiderDashboard = () => {
+    const { user } = useCurrentUser();
     const classes = useStyles();
 
     return <RiderPage selected='dashboard'>
@@ -18,13 +21,29 @@ export const RiderDashboard = () => {
         </Box>
 
         <Box
+            width='100%'
+            maxWidth='30rem'
+            marginX='auto'
+            marginBottom={2}
+        >
+            <Paper elevation={0}>
+                <Typography
+                    align='center'
+                    style={{ fontWeight: 'bold', fontSize: '1.5rem' }}
+                >
+                    $ {user.earnings}
+                </Typography>
+            </Paper>
+        </Box>
+
+        <Box
             component='div'
             display='flex'
+            flexWrap='wrap'
             width='100%'
             maxWidth='60rem'
             marginX='auto'
         >
-
         </Box>
     </RiderPage>
 }
